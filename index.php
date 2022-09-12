5574f7782a78cbaedbefab3f57f4fdcc72b1c522
Kerim Anater <kerim.anater@tum.de>
2022-09-11 18:51:31 -0700

    change bkck

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $input = file_get_contents('php://input');
 $update = json_decode($input);
 $message = $update->message;
-$chat_id = '1971171809';
+$chat_id = $message->chat->id;
 $text = $message->text;
 $token = '5671386279:AAH8R5yNUM6RoqpKtpOiaBxWWa7L2ZjoKVI';
 file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=hi");
