ad53b093a1fb35058b82cf1c75596022f6c8ee38
Kerim Anater <kerim.anater@tum.de>
2022-09-11 18:27:18 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?
+$input = file_get_contents('php://input');
+$update = json_decode($input);
+$message = $update->message;
+$chat_id = $message->chat->id;
+$text = $message->text;
+$token = '5671386279:AAH8R5yNUM6RoqpKtpOiaBxWWa7L2ZjoKVI';
+file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=hi");
